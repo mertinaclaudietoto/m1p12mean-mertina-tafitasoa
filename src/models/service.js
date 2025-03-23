41c68a216c777a63e95f46c9d7d5eac7002583ea
Tafitasoa RATSIMBAHARISON <118884621+tafitasoa1949@users.noreply.github.com>
2025-03-23 06:53:33 +0300

    fix data services

--- src/models/service.js
+++ src/models/service.js
@@ -3,29 +3,9 @@ const mongoose = require("mongoose");
 const serviceSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
-    sizeType: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "SizeType",
-      required: true,
-    },
     sizeTypePrice: { type: Number, required: true },
-    carType: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "CarType",
-      required: true,
-    },
     carTypePrice: { type: Number, required: true },
-    engineType: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "EngineType",
-      required: true,
-    },
     engineTypePrice: { type: Number, required: true },
-    weigthType: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "WeigthType",
-      required: true,
-    },
     weigthTypePrice: { type: Number, required: true },
   },
   { timestamps: true }
